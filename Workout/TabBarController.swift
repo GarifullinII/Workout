85d55f30d9dd1af0d5aa4d9e7febf53f1db38ca3
GArifullinII <ildaridrisovich@gmail.com>
2022-11-14 18:51:41 +0400

    feat: create TabBarController

---
+++ Workout/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  Workout
+//
+//  Created by Ildar Garifullin on 14/11/2022.
+//
+
+import UIKit
+
+enum Tabs: Int {
+    case overview
+    case session
+    case progress
+    case settings
+}
+
+final class TabBarController: UITabBarController {
+    
+    
+}
