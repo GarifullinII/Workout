47a1e9a16c516b6a0feff08ff85a7bc142c63296
GArifullinII <ildaridrisovich@gmail.com>
2022-11-24 16:35:17 +0400

    feat: added button

--- Workout/Controllers/Base/BaseViews/BaseInfoView.swift
+++ Workout/Controllers/Base/BaseViews/BaseInfoView.swift
@@ -17,6 +17,13 @@ class BaseInfoView: BaseView {
         return label
     }()
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .red
+        
+        return button
+    }()
+    
     private let contentView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -27,14 +34,22 @@ class BaseInfoView: BaseView {
         return view
     }()
     
-    init(with title: String? = nil, alignment: NSTextAlignment = .left) {
+    init(with title: String? = nil, buttonTitle: String? = nil) {
         titleLabel.text = title?.uppercased()
-        titleLabel.textAlignment = alignment
+        titleLabel.textAlignment = buttonTitle == nil ? .center : .left
+        
+        button.setTitle(buttonTitle, for: .normal)
+        button.isHidden = buttonTitle == nil ? true : false
+        
         super.init(frame: .zero)
     }
     
     required init?(coder: NSCoder) {
         super.init(frame: .zero)
+    }
+    
+    func addButtonTarget(target: Any?, action: Selector) {
+        button.addTarget(target, action: action, for: .touchUpInside)
     }
 }
 
@@ -43,6 +58,7 @@ extension BaseInfoView {
         super.setupViews()
         
         setupView(titleLabel)
+        setupView(button)
         setupView(contentView)
     }
     
@@ -56,6 +72,11 @@ extension BaseInfoView {
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            button.trailingAnchor.constraint(equalTo: trailingAnchor),
+            button.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 130),
+            button.heightAnchor.constraint(equalToConstant: 32),
             
             contentView.topAnchor.constraint(equalTo: contentTopAnchor, constant: contentTopOffset),
             contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
